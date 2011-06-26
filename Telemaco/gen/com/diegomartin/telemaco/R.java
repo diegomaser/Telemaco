cca261b4de579b0c3858d7377e46cd0740dd7772
Diego Martin-Serrano <diegomartinsf@gmail.com>
2011-06-26 21:28:07 +0200

    New icons for buttons in activities

--- Telemaco/gen/com/diegomartin/telemaco/R.java
+++ Telemaco/gen/com/diegomartin/telemaco/R.java
@@ -22,29 +22,32 @@ public final class R {
         public static final int normal_height=0x7f060000;
     }
     public static final class drawable {
-        public static final int ic_menu_add=0x7f020000;
-        public static final int ic_menu_delete=0x7f020001;
-        public static final int ic_menu_help=0x7f020002;
-        public static final int ic_menu_mapmode=0x7f020003;
-        public static final int ic_menu_rearrange=0x7f020004;
-        public static final int ic_menu_refresh=0x7f020005;
-        public static final int ic_menu_revert=0x7f020006;
-        public static final int ic_menu_save=0x7f020007;
-        public static final int ic_menu_share=0x7f020008;
-        public static final int ic_menu_sort_by_size=0x7f020009;
-        public static final int ic_tab_agenda_grey=0x7f02000a;
-        public static final int ic_tab_agenda_white=0x7f02000b;
-        public static final int ic_tab_compass_grey=0x7f02000c;
-        public static final int ic_tab_compass_white=0x7f02000d;
-        public static final int ic_tab_info_details_grey=0x7f02000e;
-        public static final int ic_tab_info_details_white=0x7f02000f;
-        public static final int icon=0x7f020010;
-        public static final int maleta=0x7f020011;
-        public static final int maleta2=0x7f020012;
-        public static final int maleta3=0x7f020013;
-        public static final int tab_info=0x7f020014;
-        public static final int tab_places=0x7f020015;
-        public static final int tab_plan=0x7f020016;
+        public static final int ic_button_add=0x7f020000;
+        public static final int ic_button_login=0x7f020001;
+        public static final int ic_button_register=0x7f020002;
+        public static final int ic_menu_add=0x7f020003;
+        public static final int ic_menu_delete=0x7f020004;
+        public static final int ic_menu_help=0x7f020005;
+        public static final int ic_menu_mapmode=0x7f020006;
+        public static final int ic_menu_rearrange=0x7f020007;
+        public static final int ic_menu_refresh=0x7f020008;
+        public static final int ic_menu_revert=0x7f020009;
+        public static final int ic_menu_save=0x7f02000a;
+        public static final int ic_menu_share=0x7f02000b;
+        public static final int ic_menu_sort_by_size=0x7f02000c;
+        public static final int ic_tab_agenda_grey=0x7f02000d;
+        public static final int ic_tab_agenda_white=0x7f02000e;
+        public static final int ic_tab_compass_grey=0x7f02000f;
+        public static final int ic_tab_compass_white=0x7f020010;
+        public static final int ic_tab_info_details_grey=0x7f020011;
+        public static final int ic_tab_info_details_white=0x7f020012;
+        public static final int icon=0x7f020013;
+        public static final int maleta=0x7f020014;
+        public static final int maleta2=0x7f020015;
+        public static final int maleta3=0x7f020016;
+        public static final int tab_info=0x7f020017;
+        public static final int tab_places=0x7f020018;
+        public static final int tab_plan=0x7f020019;
     }
     public static final class id {
         public static final int add=0x7f090001;
