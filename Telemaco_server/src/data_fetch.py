78375e7c69b0493169987999cff7168cfb5cc16f
Diego Martin-Serrano <diegomartinsf@gmail.com>
2011-08-10 21:39:17 +0200

    Added places of interest updates

--- Telemaco_server/src/data_fetch.py
+++ Telemaco_server/src/data_fetch.py
@@ -7,6 +7,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from telemaco.models import Country
 from telemaco.models import City
 from telemaco.models import Currency
+from telemaco.models import Place
 
 # We fetch data entity by entity
 print "Executing data_fetch process..."
@@ -14,7 +15,7 @@ print "Executing data_fetch process..."
 # Country
 print 'Getting countries information'
 countries = ws.querySPARQLtoJSON("""
-SELECT * WHERE {
+SELECT DISTINCT * WHERE {
 ?country rdf:type dbpedia-owl:Country .
 ?country rdfs:label ?name .
 ?country dbpprop:currencyCode ?currency_code .
@@ -37,8 +38,13 @@ for c in countries:
         currency, created = Currency.objects.get_or_create(code=currency_code, defaults={'name': c['currency_name']['value']})
         country.currency = currency
         country.save()
-    
-#print countries
+        
+# Currency
+currencies = Currency.objects.all()
+for currency in currencies:
+    print "Getting info for currency: ", currency.name
+    currency.rate = ws.getCurrencyExchange("EUR", currency.code)
+    currency.save()
 
 # Languages of a country
 #"""SELECT * WHERE {
@@ -55,7 +61,7 @@ for c in countries:
 for country in Country.objects.all().filter(name='Spain'):
     print 'Querying cities for country', country.name
     cities = ws.querySPARQLtoJSON("""
-    SELECT * WHERE {
+    SELECT DISTINCT * WHERE {
     ?city dbpedia-owl:country ?country .
     ?city rdfs:label ?name .
     ?city dbpedia-owl:abstract ?abstract .
@@ -66,40 +72,47 @@ for country in Country.objects.all().filter(name='Spain'):
     FILTER(LANGMATCHES(LANG(?abstract), 'EN'))
     FILTER(LANGMATCHES(LANG(?name), 'EN'))
     } ORDER BY DESC(?population)
-    LIMIT 1000
+    LIMIT 100
     """)['results']['bindings']
     
     #?country rdf:type dbpedia-owl:Country .\ 
     #?city rdf:type dbpedia-owl:PopulatedPlace . 
     #?city rdf:type dbpedia-owl:City . 
+    
     for city in cities:
         name = city['name']['value']
-        print 'Saving information for city', name, ", ", country.name
+        print 'Saving information for city', name, ',', country.name
         c, created = City.objects.get_or_create(name=name, defaults={'description':city['abstract']['value'],
                                                                      'country':country,
                                                                      'lat':city['lat']['value'],
                                                                      'lng':city['long']['value']})
         c.save()
 
-# Currency
-currencies = Currency.objects.all()
-for currency in currencies:
-    print "Getting info for currency: ", currency.name
-    currency.rate = ws.getCurrencyExchange("EUR", currency.code)
-    currency.save()
+for city in City.objects.all():
+    print 'Querying places for city', city
+    places = ws.querySPARQLtoJSON("""
+    SELECT DISTINCT * WHERE {
+    ?place rdfs:label ?name .
+    ?place dbpedia-owl:abstract ?abstract .
+    ?place geo:lat ?lat .
+    ?place geo:long ?long .
+    FILTER(xsd:double(?lat) - xsd:double("""+str(city.lat)+""") <= 0.10 &&
+    xsd:double("""+str(city.lat)+""") - xsd:double(?lat) <= 0.10 &&
+    xsd:double(?long) - xsd:double("""+str(city.lng)+""") <= 0.10 &&
+    xsd:double("""+str(city.lng)+""") - xsd:double(?long) <= 0.10 &&
+    LANGMATCHES(LANG(?name), 'EN') &&
+    LANGMATCHES(LANG(?abstract), 'EN')
+    ) } LIMIT 1000
+    """)['results']['bindings']
 
-# Language
-
-# Places near coordinates
-#"""PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> 
-#PREFIX onto: <http://dbpedia.org/ontology/> 
-#SELECT * WHERE {
-#?s a onto:Place .
-#?s geo:lat ?lat .
-#?s geo:long ?long .
-#} FILTER ( ?long > YOUR_LONG - radius && ?long < YOUR_LONG + radius &&
-#lat > YOUR_LAT - radius && ?lat < YOUR_LAT + radius)
-#LIMIT 100"""
+    for place in places:
+        name = place['name']['value']
+        print 'Saving information for place', name, 'in', city
+        p, created = Place.objects.get_or_create(name=name, defaults={'description':place['abstract']['value'], 
+                                                                      'lat': place['lat']['value'],
+                                                                      'lng': place['long']['value']})
+        p.save()
+    
 
 # Item (place)
 
